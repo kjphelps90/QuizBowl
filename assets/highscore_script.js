6783cbf3521071ebf68018dcb10007dcdc838e50
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-08-11 14:00:19 -0400

    formatted each document to make easier to comb through.

--- assets/highscore_script.js
+++ assets/highscore_script.js
@@ -5,43 +5,41 @@ var backButton = document.getElementById("back");
 function init() {
     var storedScores = JSON.parse(localStorage.getItem("High Score"));
     if (storedScores) {
-    var scoresList = JSON.parse(localStorage.getItem("High Score"));
-    scoresList.sort(function(a,b){
-        return b.score - a.score;
-    });
-    
-    if (scoresList.length < 10){
-    for (let i=0; i < scoresList.length; i++) {
-        var ranking = document.createElement("li");
-        var currentName = scoresList[i].name;
-        var currentScore = scoresList[i].score;
-        ranking.textContent= currentName + " - " + currentScore;
-        console.log(ranking);
-        highScoreList.appendChild(ranking);
-        }}
+        var scoresList = JSON.parse(localStorage.getItem("High Score"));
+        scoresList.sort(function (a, b) {
+            return b.score - a.score;
+        });
 
-    else {
-        for (let i=0; i < 10; i++) {
-            var ranking = document.createElement("li");
-            var currentName = scoresList[i].name;
-            var currentScore = scoresList[i].score;
-            ranking.textContent= currentName + " - " + currentScore;
-            console.log(ranking);
-            highScoreList.appendChild(ranking);
+        if (scoresList.length < 10) {
+            for (let i = 0; i < scoresList.length; i++) {
+                var ranking = document.createElement("li");
+                var currentName = scoresList[i].name;
+                var currentScore = scoresList[i].score;
+                ranking.textContent = currentName + " - " + currentScore;
+                console.log(ranking);
+                highScoreList.appendChild(ranking);
+            }
+        } else {
+            for (let i = 0; i < 10; i++) {
+                var ranking = document.createElement("li");
+                var currentName = scoresList[i].name;
+                var currentScore = scoresList[i].score;
+                ranking.textContent = currentName + " - " + currentScore;
+                console.log(ranking);
+                highScoreList.appendChild(ranking);
             }
         }
     }
 }
 
-clearButton.addEventListener("click", function(event) {
+clearButton.addEventListener("click", function (event) {
     localStorage.removeItem("High Score");
     location.reload();
 })
 
-backButton.addEventListener("click", function(event) {
+backButton.addEventListener("click", function (event) {
     window.location.replace("https://kjphelps90.github.io/QuizBowl/");
 })
 
 
 init();
-
--- assets/script.js
+++ assets/script.js
@@ -7,12 +7,12 @@ var logos = [
     "richmond_logo.jpg"
 ];
 var answers = [
-    ["Baylor", "California","Belmont","Chicago",2],
-    ["Auburn", "Clemson","LSU","Detroit",1],
-    ["Drexel", "Texas State","SCLSU","Montana",0],
-    ["LSU", "Virginia","Miss State","Texas",0],
-    ["Temple", "Houston","Louisville","Rice",3],
-    ["TCU", "Richmond","Oregon State","UC-Irvine",1]
+    ["Baylor", "California", "Belmont", "Chicago", 2],
+    ["Auburn", "Clemson", "LSU", "Detroit", 1],
+    ["Drexel", "Texas State", "SCLSU", "Montana", 0],
+    ["LSU", "Virginia", "Miss State", "Texas", 0],
+    ["Temple", "Houston", "Louisville", "Rice", 3],
+    ["TCU", "Richmond", "Oregon State", "UC-Irvine", 1]
 ];
 
 
@@ -64,7 +64,7 @@ submit.textContent = "Submit";
 // starting the timer after new game is clicked. Once it reaches 0 the timer goes away.
 
 function startTimer() {
-    var clock = setInterval(function(){
+    var clock = setInterval(function () {
         runningTimer--;
         timeSlot.textContent = "Time remaining: " + runningTimer;
         heading.appendChild(timeSlot);
@@ -74,8 +74,7 @@ function startTimer() {
             clearInterval(clock);
             heading.removeChild(timeSlot);
             newLabels();
-        }
-        else if (complete == 1) {
+        } else if (complete == 1) {
             score = runningTimer;
             clearInterval(clock);
         }
@@ -85,47 +84,49 @@ function startTimer() {
 
 // function to sort the high scores array and then post them on the highscores page.
 function toHighScorePage() {
-var scoresList = JSON.parse(localStorage.getItem("High Score"));
-scoresList.sort(function(a,b){
-    return b.score - a.score;
-});
+    var scoresList = JSON.parse(localStorage.getItem("High Score"));
+    scoresList.sort(function (a, b) {
+        return b.score - a.score;
+    });
 
-for (let i=0; i < scoresList.length; i++) {
-    var ranking = document.createElement("li");
-    var currentName = scoresList[i].name;
-    var currentScore = scoresList[i].score;
-    ranking.textContent= currentName + " - " + currentScore;
-    console.log(ranking);
-}}
+    for (let i = 0; i < scoresList.length; i++) {
+        var ranking = document.createElement("li");
+        var currentName = scoresList[i].name;
+        var currentScore = scoresList[i].score;
+        ranking.textContent = currentName + " - " + currentScore;
+        console.log(ranking);
+    }
+}
 
 // function that post scores to the local storage.
 function postScores() {
     var name = initialsInput.value;
-    var submitScores = {"name": name,"score": score};
+    var submitScores = {
+        "name": name,
+        "score": score
+    };
     var storedScores = JSON.parse(localStorage.getItem("High Score"));
 
-    if (!storedScores){
+    if (!storedScores) {
         var highScores = [];
         highScores.push(submitScores);
-    }
-    else {
-    highScores = storedScores;
-    highScores.push(submitScores);    
+    } else {
+        highScores = storedScores;
+        highScores.push(submitScores);
     }
     localStorage.setItem("High Score", JSON.stringify(highScores));
 }
 
 //image starts at 0, when the page loads. This function sets the first question when the 'New Game' button is hit. The else statement is for when we've run out of images, the prompt to enter intials for high score comes up.
 function newLabels() {
-    if (image < logos.length && runningTimer > 0){
-        logoEL.src="./assets/images/" + logos[image];
-        answer1.textContent=answers[image][0];
-        answer2.textContent=answers[image][1];
-        answer3.textContent=answers[image][2];
-        answer4.textContent=answers[image][3];
+    if (image < logos.length && runningTimer > 0) {
+        logoEL.src = "./assets/images/" + logos[image];
+        answer1.textContent = answers[image][0];
+        answer2.textContent = answers[image][1];
+        answer3.textContent = answers[image][2];
+        answer4.textContent = answers[image][3];
         image++;
-    }
-    else {
+    } else {
         complete = 1;
         main.removeChild(logoEL);
         list.remove();
@@ -139,26 +140,25 @@ function newLabels() {
 }
 // Function to check if answers are correct. The answers are stored at the end of the 'answers' array. The selection is set in the EventListeners on the bottom to compare.
 function answerCheck() {
-    if (selection === answers[image-1][4]) {
-    reply.textContent = "You are correct, well done!";
-    }
-    else {
-    reply.textContent = "That is WRONG";
-    runningTimer -= 10;
+    if (selection === answers[image - 1][4]) {
+        reply.textContent = "You are correct, well done!";
+    } else {
+        reply.textContent = "That is WRONG";
+        runningTimer -= 10;
     }
 }
 
 // EvenListener for the New Game button. It changes the image to the first one in the array. It also sets up a list with buttons to be used for the game.
-newGame.addEventListener("click", function(event){
+newGame.addEventListener("click", function (event) {
     startTimer();
     question.textContent = "Which college or university does the following logo represent?"
-    image=0;
-    logoEL.src="./assets/images/" + logos[image];
+    image = 0;
+    logoEL.src = "./assets/images/" + logos[image];
     main.appendChild(question);
-    answer1.textContent=answers[0][0];
-    answer2.textContent=answers[0][1];
-    answer3.textContent=answers[0][2];
-    answer4.textContent=answers[0][3];
+    answer1.textContent = answers[0][0];
+    answer2.textContent = answers[0][1];
+    answer3.textContent = answers[0][2];
+    answer4.textContent = answers[0][3];
     reply.textContent = "Please make a selection.";
     list.appendChild(item1).appendChild(answer1);
     list.appendChild(item2).appendChild(answer2);
@@ -168,38 +168,37 @@ newGame.addEventListener("click", function(event){
 })
 
 // The following 4 EventListeners are assigned to each of the buttons that are created when the New Game is setup.
-answer1.addEventListener("click", function(event) {
-    selection=0;
+answer1.addEventListener("click", function (event) {
+    selection = 0;
     answerCheck();
     newLabels();
 })
 
-answer2.addEventListener("click", function(event) {
-    selection=1;
+answer2.addEventListener("click", function (event) {
+    selection = 1;
     answerCheck();
     newLabels();
 })
 
-answer3.addEventListener("click", function(event) {
-    selection=2;
+answer3.addEventListener("click", function (event) {
+    selection = 2;
     answerCheck();
     newLabels();
 })
 
-answer4.addEventListener("click", function(event) {
-    selection=3;
+answer4.addEventListener("click", function (event) {
+    selection = 3;
     answerCheck();
     newLabels();
 })
 
 // This EventListener is setup to try and setup a HighScores page.
-submit.addEventListener("click", function(event){
+submit.addEventListener("click", function (event) {
     postScores();
     window.location.replace("https://kjphelps90.github.io/QuizBowl/highscore.html");
 
 })
 
-btnHighScore.addEventListener("click", function() {
+btnHighScore.addEventListener("click", function () {
     window.location.replace("https://kjphelps90.github.io/QuizBowl/highscore.html");
 })
-
